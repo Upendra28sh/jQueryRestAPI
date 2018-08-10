ca9c4111ac73f4a7b79911e0cf844c4621912002
upendra <upendra861@gmail.com>
2018-08-10 20:27:29 +0530

    Files Updated

---
+++ script.js
@@ -0,0 +1,152 @@
+function getUsers(done) {
+    $.getJSON('https://jsonplaceholder.typicode.com/users', function (data) {
+        //JSON.stringify(data);
+        done(data);
+    })
+}
+
+function getAlbums(done) {
+    $.getJSON('https://jsonplaceholder.typicode.com/albums', function (data) {
+        // JSON.stringify(data);
+        done(data);
+    })
+}
+
+function getPosts(done) {
+    $.getJSON('https://jsonplaceholder.typicode.com/posts', function (data) {
+        // JSON.stringify(data);
+        done(data);
+    })
+}
+
+function getTodos(done) {
+    $.getJSON('https://jsonplaceholder.typicode.com/todos', function (data) {
+        // JSON.stringify(data);
+        done(data);
+    })
+}
+
+function toggleactive(toggle) {
+    $('.nav > li > a').removeClass('active');
+    $(`.tab-${toggle} > a`).addClass('active');
+    $(`.contents`).hide();
+    $(`.${toggle}-contents`).show();
+}
+
+function refreshUsers(users) {
+    users.forEach(
+        function (user) {
+            $('#users-clicked').append(
+                `<div class="col-sm-6 col-md-6 col-lg-4 p-3">
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">${user.name}</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">${user.email}</h6>
+                    <p class="card-text">
+                    Address :- <br>
+                    ${user.address.street}
+                    ${user.address.suite}<br>
+                    ${user.address.city}
+                    ${user.address.zipcode}<br>
+                    </p>
+                    <a href="#" class="card-link">${user.phone}</a><br>
+                    <a href="#" class="card-link">${user.website}</a>
+                </div>
+            </div>
+            </div>
+            `
+            )
+        }
+    )
+}
+
+function refreshAlbums(albums) {
+    albums.forEach(
+        function (album) {
+            $('#albums-clicked').append(
+                `<div class="row list-group-item">
+                <span>
+                ${album.title}
+                </span>
+                </div>
+            `
+            )
+        }
+    )
+}
+
+function refreshPosts(posts) {
+    posts.forEach(
+        function (post) {
+            $('#posts-clicked').append(
+                `<div class="col-sm-6 col-md-6 col-lg-4 p-3">
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">${post.title}</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">${post.body}</h6>
+                    <p class="card-text">
+                    
+                    </p>
+                    <a href="#" class="card-link"></a><br>
+                    <a href="#" class="card-link"></a>
+                </div>
+            </div>
+            </div>
+            `
+            )
+        }
+    )
+}
+
+function refreshTodos(todos) {
+    todos.forEach(
+        function (todo) {
+            $('#todos-clicked').append(
+                `
+                <div class="row list-group-item">
+                <span>
+                ${todo.title}
+                </span>
+                </div>
+                
+            `
+            )
+        }
+    )
+}
+
+$(function () {
+    $('.tab-users').click(function () {
+        toggleactive('users')
+        getUsers(function (users) {
+            refreshUsers(users);
+        })
+    })
+})
+
+$(function () {
+    $('.tab-albums').click(function () {
+        toggleactive('albums')
+        getAlbums(function (albums) {
+            refreshAlbums(albums);
+        })
+    })
+})
+
+$(function () {
+    $('.tab-posts').click(function () {
+        toggleactive('posts')
+        getPosts(function (posts) {
+            refreshPosts(posts);
+        })
+    })
+})
+
+$(function () {
+    $('.tab-todos').click(function () {
+        toggleactive('todos')
+        getTodos(function (todos) {
+            refreshTodos(todos);
+        })
+    })
+})
